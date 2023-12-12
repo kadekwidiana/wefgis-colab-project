c7fd26bb6c51abfafbb0bf96196c0b4e562e39f5
darmawguna <komangagus642@gmail.com>
2023-12-12 15:41:46 +0800

    update login

--- resources/views/backpage/layouts/navbar.blade.php
+++ resources/views/backpage/layouts/navbar.blade.php
@@ -91,13 +91,16 @@
                     <!-- end item -->
 
                     <hr>
-
                     <!-- item -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf 
                     <a class="px-4 py-2 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 hover:text-gray-900 transition-all duration-300 ease-in-out"
-                        href="#">
+                        href="/" onclick="event.preventDefault();
+                                                this.closest('form').submit();">
                         <i class="fad fa-user-times text-xs mr-1"></i>
                         log out
                     </a>
+                    </form>
                     <!-- end item -->
 
                 </div>
